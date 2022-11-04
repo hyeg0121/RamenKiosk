738653b49f8ec5148d005fb026273ba271fb931f
hyeg0121 <hyeg0121@naver.com>
2022-11-04 20:43:16 +0900

    상속할 프레임 클래스 생성

---
+++ JavaProject/src/kiosk/CommonFrame.java
@@ -0,0 +1,15 @@
+package kiosk;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class CommonFrame extends JFrame {
+		public CommonFrame() {
+			setSize(450, 800);
+			setLayout(null);
+			setBackground(Color.white);
+			setLocationRelativeTo(null);
+			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		}
+}
